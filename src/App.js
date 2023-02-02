a3702edf29b0673119cb655fc0f013e9099eaeaa
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-02-02 12:51:35 +0100

    add testData

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
-import radwege from './cyclePath.geojson';
+import testData from './testData.json';
+
 
 function App() {
 
   const position = [51.3396955, 12.3730747];
-
+  
   const showCycle = () => {
     console.log('Button wurde gedrückt!'); 
   }
@@ -19,15 +20,14 @@ function App() {
         attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
       />
+      <GeoJSON attribution="&copy; credits due..." data={testData} />>
     </MapContainer>
     <div className='button'>
       <button id='btn1' onClick={showCycle}>Radverkehr</button>
     </div>
+    <div id='hinweise'>Legende aus cyclosm.org übernehmen</div>
     </main>
   );
 }
 
 export default App;
-
-
-//<GeoJSON data={radwege} /> nach TileLayer
