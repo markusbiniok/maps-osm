9cbcd2246bce1f304f5d2393f4d121a56890a7d8
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-02-13 14:35:57 +0100

    try to customize marker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, GeoJSON, Marker } from 'react-leaflet';
+import L from 'leaflet';
 import './App.css';
 import testData from './testData.json';
-
 
 function App() {
 
   const position = [51.3396955, 12.3730747];
+
   
-  const showCycle = () => {
+  const showCycleTraffic = () => {
     console.log('Button wurde gedrückt!'); 
+  }
+
+  const markerStyle = {
+    backgroundColor: "blue",
+    width: "10px",
+    height: "10px",
+    borderRadius: "50px"
   }
 
   return (
@@ -20,10 +27,10 @@ function App() {
         attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
       />
-      <GeoJSON attribution="&copy; credits due..." data={testData}/>
+      <GeoJSON data={testData} icon={markerStyle}/>
     </MapContainer>
     <div className='button'>
-      <button id='btn1' onClick={showCycle}>Radverkehr</button>
+      <button id='btn1' onClick={showCycleTraffic}>Radverkehr</button>
     </div>
     <div id='hinweise'>Legende aus cyclosm.org übernehmen</div>
     </main>
