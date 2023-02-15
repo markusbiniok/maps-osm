96ede1a981a24ea1b586b5db95a9b7cc95af5b16
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-02-15 12:19:14 +0100

    add legend

--- src/App.js
+++ src/App.js
@@ -21,14 +21,33 @@ function App() {
   }
 
   return (
+    
     <main id='main'>
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
-      />
-      <GeoJSON data={testData} icon={markerStyle}/>
-    </MapContainer>
+      <div className='mapPart'>
+        <div className='map'>
+          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
+            />
+            <GeoJSON data={testData} icon={markerStyle}/>
+          </MapContainer>
+        </div>
+        <div className='legend'>
+          Legende
+          <td>
+            <tr>
+              eigener Radweg
+            </tr>
+            <tr>
+              Hallo
+            </tr>
+            <tr>
+              Hallo
+            </tr>
+          </td>
+        </div>
+      </div>
     <div className='button'>
       <button id='btn1' onClick={showCycleTraffic}>Radverkehr</button>
     </div>
