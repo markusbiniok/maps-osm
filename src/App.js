66b23cd7c82c776767a76e14300f2f51a960452b
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-02-17 12:00:43 +0100

    add CircleMarker

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
 import React from 'react';
-import { MapContainer, TileLayer, GeoJSON, Marker } from 'react-leaflet';
-import L from 'leaflet';
+import { MapContainer, TileLayer, GeoJSON, CircleMarker } from 'react-leaflet';
+import L, { map } from 'leaflet';
 import './App.css';
 import testData from './testData.json';
 
 function App() {
 
-  const position = [51.3396955, 12.3730747];
+  const mapCenter = [51.3396955, 12.3730747];
 
   
   const showCycleTraffic = () => {
     console.log('Button wurde gedrückt!'); 
   }
 
-  const markerStyle = {
-    backgroundColor: "blue",
-    width: "10px",
-    height: "10px",
-    borderRadius: "50px"
-  }
-
+  
   return (
     
     <main id='main'>
       <div className='mapPart'>
         <div className='map'>
-          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
+          <MapContainer center={mapCenter} zoom={13} scrollWheelZoom={false}>
+            <TileLayer id='map'
               attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
             />
-            <GeoJSON data={testData} icon={markerStyle}/>
+            <GeoJSON data={testData}/>
+            <CircleMarker center={[51.3396955, 12.3730747]}/>
           </MapContainer>
-        </div>
-        <div className='legend'>
-          Legende
-          <td>
-            <tr>
-              eigener Radweg
-            </tr>
-            <tr>
-              Hallo
-            </tr>
-            <tr>
-              Hallo
-            </tr>
-          </td>
         </div>
       </div>
     <div className='button'>
