d81ef0ef12a78a9a1f3375d3d889062f344ab9e4
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-02-21 14:11:37 +0100

    mapCenter and cycleTraffic on Button workgit  add .!

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback, useState, useMemo } from 'react';
 import L, { map } from 'leaflet';
-import { LayersControl, Popup, MapContainer, TileLayer, GeoJSON, Marker, useMap } from 'react-leaflet';
+import { LayersControl, Popup, MapContainer, TileLayer, GeoJSON, Marker, useMap, Circle, CircleMarker, useMapEvent } from 'react-leaflet';
 import './App.css';
 import testData from './testData.json';
-import cycleMarker from './circle.png';
 
 
-function App() {
+const mapCenter = [51.3396955, 12.3730747];
+const mapZoom = 13;
 
-  const mapRef = useRef();
-  const center = [51.3396955, 12.3730747];
 
-  const customIcon1 = L.icon ({
-    iconUrl: require('./circle.png'),
-    iconSize: [15, 15]
-  });
+//from https://react-leaflet.js.org/docs/example-external-state/
+function DisplayPosition({ map }) {
 
-  const centerMap = () => {
-    console.log('Button wurde gedrückt');
-  }
+  const onClick = useCallback(() => {
+    map.setView(mapCenter, mapZoom)
+  }, [map]);
+
+  return (
+    <p>
+      <button onClick={onClick}>Karte zentrieren</button>
+    </p>
+  )
+}
+
+function DisplayCycleTraffic({ map }) {
   
-
-  useEffect(() => {
-    const map = mapRef.current;
-    if (!map) return;
-
+  const onClick = useCallback(() => {
     var markerStyle = {
-      radius: 2,
+      radius: 1,
       fillColor: '#ff0000',
       fillOpacity: 1,
       color: '#ff0000',
-      weight: 1, 
+      weight: 2, 
       opacity: 1
     }
   
@@ -39,7 +40,19 @@ function App() {
         return L.circleMarker (latlng, markerStyle);
       }
     }).addTo(map);
-  }, []);
+  }, [map]);
+
+  return (
+    <p>
+      <button onClick={onClick}>Radverkehr</button>
+    </p>
+  )
+}
+
+
+
+function App() {
+  const [map, setMap] = useState(null);
 
   const showCycleTraffic = () => {
     console.log('Button wurde gedrückt');
@@ -47,33 +60,51 @@ function App() {
 
   return (
     <main id='main'>
+      <div className='header'>
+        Radverkehrsdaten und Radwegnutzung
+      </div>
       <div className='mapPart'>
         <div className='map'>
-        <MapContainer ref={mapRef} center={center} zoom={13} scrollWheelZoom={false}>
-            <TileLayer id='map'
-              attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.35, 12.3730747]} icon={customIcon1}/> 
+        <MapContainer ref={setMap} center={mapCenter} zoom={mapZoom} scrollWheelZoom={false}>
+          <TileLayer id='map'
+            attribution='&copy; <a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
+          />
+          <CircleMarker center={[51.3396955, 12.376]} /> 
         </MapContainer>
         </div>
-        <div className='buttons'>
-          <div className='button1'>
-            <button id='btn1' onClick={showCycleTraffic}>Radverkehr</button>
-          </div>
-          <div className='button2'>
-            <button id='btn2' onClick={centerMap}>Map zentrieren</button>
+        <div className='legend'>
+          <p id='caption'>Legende</p>
+          <p id='ut'>Radinfrastruktur</p>
+          <div className='pathOptions'>
+            <p><span id='span1'/> Radweg</p>
+            <p><span id='span4'/> ausgewiesener Radweg</p>
+            <p><span id='span2'>
+            <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                  <line x1="10%" x2="150%" y1="10%" y2="10%" strokeLinecap="butt" strokeDasharray="6,3" stroke="#0000ce" strokeWidth="20%"></line>
+                  <line x1="10%" x2="150%" y1="70%" y2="70%" strokeLinecap="butt" stroke="#888" strokeWidth="10%"></line>
+                  <rect x="10%" y="10%" width="140%" height="60%" fill="#e3e1e1"></rect>
+              </svg>
+            </span> Radfahrstreifen</p>
+            <p><span id='span3'/> Fahrradstraße</p>
           </div>
         </div>
+        </div>
+        <div className='buttons'> 
+          <div id='btn1'>
+            {map ? <DisplayCycleTraffic map={map} /> : null}        
+          </div>
+          <div id='btn2'>
+            {map ? <DisplayPosition map={map} /> : null}        
+          </div>
+        </div>
+      <div id='hinweise'>
+        Legende aus cyclosm.org übernehmen
       </div>
-    <div id='hinweise'>Legende aus cyclosm.org übernehmen</div>
     </main>
-  );
-  
+  )
 }
 
 export default App;
 
 //<CircleMarker center={[51.3396955, 12.3730747]} style={circleStyle}/>
-
-//<Marker position={[51.35, 12.3730747]} icon={customIcon1}/> 
