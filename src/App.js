47b4d557ec6d9e6e250c75d133a0520cee31461e
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-02-22 12:09:44 +0100

    add grid to legend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import './App.css';
+import bicycle1 from './bicycle1.svg';
+import testData from './testData.json';
+
 import React, { useCallback, useState } from 'react';
 import L from 'leaflet';
 import { LayersControl, MapContainer, TileLayer, GeoJSON, CircleMarker, ZoomControl } from 'react-leaflet';
-import './App.css';
-import testData from './testData.json';
-import bicycle1 from './bicycle1.svg';
+
+
 
 const mapCenter = [51.3396955, 12.3730747];
 const mapZoom = 14;
-
 
 //show cycle traffic
 function DisplayCycleTraffic({ map }) {
