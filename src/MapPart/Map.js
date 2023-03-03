3688815372624872c9cc05a9284d4a15a7c4404c
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-03-03 13:10:07 +0100

    toggleButton for cycleTraffic --> only one button

--- src/MapPart/Map.js
+++ src/MapPart/Map.js
@@ -10,71 +10,11 @@ import { MapContainer, TileLayer, ZoomControl, Marker, LayersControl, LayerGroup
 const mapCenter = [51.336, 12.3730747];
 const mapZoom = 14;
 
+
 //show cycle traffic
 function DisplayCycleTraffic({ map }) {
   
-  const showCycleTraffic = useCallback(() => {
-    var markerStyle = {
-      radius: 1,
-      fillColor: '#ff0000',
-      fillOpacity: 1,
-      color: '#ff0000',
-      weight: 2, 
-      opacity: 1
-    }
-
-    //geoJSON points to mapLayer
-    L.geoJSON (testData, {
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker (latlng, markerStyle);
-      }
-    }).addTo(map);
-  }, [map]);
-
-  return (
-  
-    <div className='button'>
-      <button id='mapBtn1' onClick={showCycleTraffic}>Radverkehr</button> 
-    </div>
-  )
-}
-
-//from https://react-leaflet.js.org/docs/example-external-state/
-//center map
-function DisplayPosition({ map }) {
-
-  const centerMap = useCallback(() => {
-    map.setView(mapCenter, mapZoom)
-  }, [map]);
-  
-  return (
-    <p>
-      <button id='mapBtn2' onClick={centerMap}>Karte zentrieren</button>
-    </p>
-  )
-}
-
-function Map() {
-
-  const [map, setMap] = useState(null); 
-  const dzs1 = [51.339290, 12.345644]; //Jahnallee
-  const dzs2 = [51.346394, 12.376698]; //Gerberstraße
-  const dzs3 = [51.326745, 12.373747]; //Karl-Liebknecht-Straße
-  const dzs4 = [51.334458, 12.399261]; //Lene-Voigt-Park
-  const dzs5 = [51.335879, 12.367335]; //Manetstraße
-  const dzs6 = [51.320706, 12.386071]; //Semmelweisstraße
-
-  /*var heatmap = h337.create({
-    container: document.getElementsByClassName('map')
-  });
-
-  heatmap.setData ({
-    max: 5,
-    data: [{x: 10, y: 15, value: 5}]
-  });*/
-
   const cycleTraffic = useCallback(() => {
-    console.log('Button gedrückt');
     var el = document.getElementById('cb1');
     switch (el.checked) {
       case true:
@@ -105,6 +45,87 @@ function Map() {
     }
   }, [map]);
 
+  return (
+  
+    <div className='mapBtn1'>
+      <label className="switch">
+        <input type="checkbox" id="cb1" onClick={cycleTraffic}/>
+        <span className="slider round"/>
+      </label>
+      <p>Radverkehr</p>
+    </div>
+  )
+}
+
+function DisplayDzs({ map }) {
+  
+  const dzs = useCallback(() => {
+    var el = document.getElementById('cb2');
+    switch (el.checked) {
+      case true:
+        //method to show marker of all dzs
+        break;
+      case false:
+        //method to remove all marker of all dzs
+        map.eachLayer(function (layer) {
+          if (layer instanceof L.Marker) {
+            layer.remove()
+          }
+        });
+        break;
+      default:
+        break;
+    }
+  }, [map]);
+
+  return (
+  
+    <div className='mapBtn3'>
+      <label className="switch">
+        <input type="checkbox" id="cb2" onClick={dzs}/>
+        <span className="slider round"/>
+      </label>
+      <p>Dauerzählstellen</p>
+    </div>
+  )
+}
+
+//from https://react-leaflet.js.org/docs/example-external-state/
+//center map
+function DisplayPosition({ map }) {
+
+  const centerMap = useCallback(() => {
+    map.setView(mapCenter, mapZoom)
+  }, [map]);
+  
+  return (
+    <div className='mapBtn2'>
+      <button id='btn2' onClick={centerMap}>Karte zentrieren</button>
+    </div>
+  )
+}
+
+function Map() {
+
+  const [map, setMap] = useState(null); 
+  const dzs1 = [51.339290, 12.345644]; //Jahnallee
+  const dzs2 = [51.346394, 12.376698]; //Gerberstraße
+  const dzs3 = [51.326745, 12.373747]; //Karl-Liebknecht-Straße
+  const dzs4 = [51.334458, 12.399261]; //Lene-Voigt-Park
+  const dzs5 = [51.335879, 12.367335]; //Manetstraße
+  const dzs6 = [51.320706, 12.386071]; //Semmelweisstraße
+
+  /*var heatmap = h337.create({
+    container: document.getElementsByClassName('map')
+  });
+
+  heatmap.setData ({
+    max: 5,
+    data: [{x: 10, y: 15, value: 5}]
+  });*/
+
+
+
 
   return (
     <main id='mainPart'>
@@ -133,18 +154,14 @@ function Map() {
         <Legend/>
       </div>
       <div className='mapButtons'>
-        <div className='mapButton1'>
+        <div>
           {map ? <DisplayCycleTraffic map={map} /> : null}   
         </div> 
-        <div className='mapButton2'> 
+        <div>
+          {map ? <DisplayDzs map={map} /> : null}   
+        </div> 
+        <div> 
           {map ? <DisplayPosition map={map} /> : null}  
-        </div>
-        <div className='mapButton3'> 
-          <label className="switch">
-            <input type="checkbox" id="cb1" onClick={cycleTraffic}/>
-            <span className="slider round"/>
-          </label>
-          <p id="cycle">Radverkehr</p>
         </div>
       </div>
     </main>
