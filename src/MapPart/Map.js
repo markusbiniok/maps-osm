d43005ffc41c2111be6caec84dc1eb83c7733418
Markus Biniok <markus.biniok@stud.htwk-leipzig.de>
2023-04-17 14:13:24 +0200

    error: HeatmapOverlay not found

--- src/MapPart/Map.js
+++ src/MapPart/Map.js
@@ -271,10 +271,38 @@ function DisplayHeatmap({ map }) {
 
     switch (document.getElementById('cb4').checked) {
       case true:
-        console.log('true');  
+        console.log('true'); 
+
+        var testData = {
+          max: 8,
+          data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 4}]
+        };
+
+        var cfg = {
+          // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+          // if scaleRadius is false it will be the constant radius used in pixels
+          "radius": 1,
+          "maxOpacity": .8,
+          // scales the radius based on map zoom
+          "scaleRadius": true,
+          // if set to false the heatmap uses the global maximum for colorization
+          // if activated: uses the data maximum within the current map boundaries
+          //   (there will always be a red spot with useLocalExtremas true)
+          "useLocalExtrema": true,
+          // which field name in your data represents the latitude - default "lat"
+          latField: 'lat',
+          // which field name in your data represents the longitude - default "lng"
+          lngField: 'lng',
+          // which field name in your data represents the data value - default "value"
+          valueField: 'count'
+        };
+
+        var heatmapLayer = new HeatmapOverlay(cfg);
+
+        heatmapLayer.setData(testData);
         /*L.heatLayer(
           [[50.5, 30.5, 0.2],[50.6, 30.4, 0.5]], {radius: 25}
-        ).addTo(map);*/       
+        ).addTo(map);*/ 
         break;
       case false:
         console.log('false');
