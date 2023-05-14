77b705caa7f914e1e2e7ef6fe5dea3028bd2a95e
mbiniok <YJP6F64q>
2023-05-14 21:36:41 +0200

    add legend for heatmap

--- src/MapPart/Legend.js
+++ src/MapPart/Legend.js
@@ -10,7 +10,7 @@ function Legend() {
       <p id='ut'>Radinfrastruktur</p>
       <div className='pathOptions'>
         <div id='span1'/><div id='p1'>Radweg</div> 
-        <div id='span2'/><div id='p2'>Radweg evtl. mit FG</div> 
+        <div id='span2'/><div id='p2'>gemeinsamer Rad- und Fußweg</div> 
         <div id='span3'>
           <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
               <line x1="10%" x2="150%" y1="10%" y2="10%" strokeLinecap="butt" strokeDasharray="6,3" stroke="#0000ce" strokeWidth="20%"></line>
@@ -32,9 +32,15 @@ function Legend() {
         <div id='span10'/><div id='p10'>regionale Radrouten</div> 
         <div id='span11'/><div id='p11'>lokale Radrouten</div>                     
       </div>
+      <p id='ut'>Heatmap</p>
+      <div className='heatmap'>
+        <div id='high'/><div id='textHigh'>hohe Auslastung</div>
+        <div id='middle'/><div id='textMiddle'>mittlere Auslastung</div>   
+        <div id='low'/><div id='textLow'>niedrige Auslastung</div>                
+      </div>
       <p id='ut'>Links</p>
       <div className='hyperlink'>
-        <a id='link1' href="https://geoportal.leipzig.de/arcgis/apps/experiencebuilder/experience/?id=4fd2c688fa754d55903dcb8acf9dafa7&page=page_0" target='_blank' rel='noopener noreferrer'>Dauerzählstellen Leipzig</a>                    
+        <a id='link1' href="https://geoportal.leipzig.de/arcgis/apps/experiencebuilder/experience/?id=4fd2c688fa754d55903dcb8acf9dafa7&page=page_0" target='_blank' rel='noopener noreferrer'>Dauerzählstellen Stadt Leipzig</a>                    
       </div>
     </div>
     )
